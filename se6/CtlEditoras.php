e9e40ddbcada325ae6a9e8368eaf56abfa0b6d38
Gilson Saturnino <gilsonsatu@gmail.com>
2021-03-24 14:22:58 -0400

    SE6

---
+++ se6/CtlEditoras.php
@@ -0,0 +1,24 @@
+<?php
+    $host = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $bd = "biblioteca";
+    $conexao = new mysqli($host, $usuario, $senha, $bd);
+    $sql = "insert into editoras(nome) values(?)";
+    $sqlprep = $conexao->prepare($sql);
+    $sqlprep->bind_param("s", $_POST["nome"]);
+    $sqlprep->execute();
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h4>Editora salva.</h4>
+        </div>
+    </body>
+</html>
+
+ 
---
+++ se6/CtlLivro.php
@@ -0,0 +1,62 @@
+<?php
+    $msg = "";
+    if ($_POST["titulo"]=="") {
+        $msg = "Título não prenchido. ";
+    }
+    if ($_POST["qtdExemplares"]=="") {
+        $msg = $msg . "Quantidade de Exemplares não preenchido. ";
+    }
+    if ($_POST["descricao"]=="") {
+        $msg = $msg . "Descrição não preenchido.";
+    }
+    if($msg=="") {
+        $msg = "O livro foi salvo.";
+    }
+
+    $host = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $bd = "biblioteca";
+    $conexao = new mysqli($host, $usuario, $senha, $bd);
+    $sql = "insert into livro(titulo, autor, editora, descricao, qtdexemplares)";
+    $sql = $sql . " values(?,?,?,?,?)";
+    $sqlprep = $conexao->prepare($sql);
+    $sqlprep->bind_param("ssssi", 
+                        $_POST["titulo"],
+                        $_POST["autor"],
+                        $_POST["editora"],
+                        $_POST["descricao"],
+                        $_POST["qtdExemplares"]
+    );
+    $sqlprep->execute();
+
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <table class="table table-striped">
+                <tr>
+                    <th>Título</th>
+                    <th>Descrição</th>
+                    <th>Qtde Exemplares</th>
+                    <th>Editora</th>
+                    <th>Autor</th>
+                </tr>
+                <tr>
+                    <td><?php echo $_POST["titulo"]; ?></td>
+                    <td><?php echo $_POST["descricao"]; ?></td>
+                    <td><?php echo $_POST["qtdExemplares"]; ?></td>
+                    <td><?php echo $_POST["editora"]; ?></td>
+                    <td><?php echo $_POST["autor"]; ?></td>
+                </tr>
+            </table>
+            <h4> <?php echo $msg; ?> </h4>
+        </div>
+    </body>
+</html>
+
+ 
